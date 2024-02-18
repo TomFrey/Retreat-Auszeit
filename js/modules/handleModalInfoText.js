d41537955d4a9728b030c2fc4b324739f3d7ddec
Thomas Frey <joe@mitlinxlernen.ch>
2024-02-18 19:25:18 -0300

    Inhalte von Detailseite Zimmer hinzugefügt.

--- js/modules/handleModalInfoText.js
+++ js/modules/handleModalInfoText.js
@@ -1,16 +1,6 @@
-//const OPEN_OVERLAY = 'js-openOverlayDetail';
 const dialog = document.querySelector('dialog.modal-detail-info');
 const modalCloseButton = document.querySelector('.modal-detail-info-close-button');
 const  openModalButtons = document.querySelectorAll('.openModalWithInfoText');
-
-// function toggleModal(event) {
-//     console.log('overlay geöffnet ' + event);
-//     overlay.classList.toggle(OPEN_OVERLAY);
-// }
-
-// function closeModal(event) {
-//     toggleModal(event.target);
-// }
 
 function init() {
     if (modalCloseButton !== null) {
@@ -19,8 +9,6 @@ function init() {
         });
     }
 
-    //open modal with info text
-   // openModalButtons = document.querySelectorAll('.openModalWithInfoText');
     if (openModalButtons !== null) {
         openModalButtons.forEach((openModalButton) => {
             openModalButton.addEventListener('click', (event) => {
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './scss/main.scss'
 
+//import {linkToUnCryptMailto} from './js/utils/crypto.js'
 import { init as initHandleModalInfoText } from './js/modules/handleModalInfoText.js'
 import { init as initImageSlider } from './js/modules/imageSlider.js'
 
